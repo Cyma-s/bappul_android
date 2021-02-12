7d27fbbe88fa0882e096b4c2019d226e76779a63
SeongSoo Jeon <stome159@naver.com>
2021-02-13 00:41:03 +0900

    주변 음식점 표시

--- app/src/main/java/com/example/clug_bobple/Map.java
+++ app/src/main/java/com/example/clug_bobple/Map.java
@@ -1,6 +1,7 @@
 package com.example.clug_bobple;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.app.FragmentManager;
 import android.os.Bundle;
@@ -10,10 +11,22 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends AppCompatActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
+import noman.googleplaces.NRPlaces;
+import noman.googleplaces.Place;
+import noman.googleplaces.PlaceType;
+import noman.googleplaces.PlacesException;
+import noman.googleplaces.PlacesListener;
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback, PlacesListener {
+    List<Marker> previous_marker = new ArrayList<>();
+    GoogleMap mMap;
     private FragmentManager fragmentManager;
     private MapFragment mapFragment;
 
@@ -30,12 +43,74 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
         LatLng location = new LatLng(37.50532641259903, 126.95701536932249);
         MarkerOptions marker = new MarkerOptions();
         marker.title("중앙대학교");
         marker.position(location);
-        googleMap.addMarker(marker);
+        mMap.addMarker(marker);
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+        showPlaceInformation(location);
+    }
+
+    @Override
+    public void onPlacesFailure(PlacesException e) {
+
+    }
+
+    @Override
+    public void onPlacesStart() {
+
+    }
+
+    @Override
+    public void onPlacesSuccess(List<Place> places) {
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (noman.googleplaces.Place place : places) {
+
+                    LatLng latLng
+                            = new LatLng(place.getLatitude()
+                            , place.getLongitude());
+
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(latLng);
+                    markerOptions.title(place.getName());
+                    Marker item = mMap.addMarker(markerOptions);
+                    previous_marker.add(item);
+
+                }
+
+                //중복 마커 제거
+                HashSet<Marker> hashSet = new HashSet<Marker>();
+                hashSet.addAll(previous_marker);
+                previous_marker.clear();
+                previous_marker.addAll(hashSet);
+            }
+        });
+    }
+
+    @Override
+    public void onPlacesFinished() {
+
+    }
+    public void showPlaceInformation(LatLng location)
+    {
+        mMap.clear();//지도 클리어
+
+        if (previous_marker != null)
+            previous_marker.clear();//지역정보 마커 클리어
+
+        new NRPlaces.Builder()
+                .listener(Map.this)
+                .key("AIzaSyCKx9eXDOXbFukMXevz0wTO3wSxyXGoY8A")
+                .latlng(location.latitude, location.longitude)//현재 위치
+                .radius(500) //500 미터 내에서 검색
+                .type(PlaceType.RESTAURANT) //음식점
+                .build()
+                .execute();
     }
 }
