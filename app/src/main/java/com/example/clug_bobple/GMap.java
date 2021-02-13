620633009e62d2e51a20fb21b5fc1cfa92f387cf
JaeHoon Kim <choco730522@gmail.com>
2021-02-13 16:14:26 +0900

    클래스이름 GMap으로 바꿈

--- app/src/main/java/com/example/clug_bobple/GMap.java
+++ app/src/main/java/com/example/clug_bobple/GMap.java
@@ -34,7 +34,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-//import java.util.Map;
 
 import noman.googleplaces.NRPlaces;
 import noman.googleplaces.Place;
