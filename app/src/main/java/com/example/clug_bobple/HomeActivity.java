80126da81e19612b5033dfed47b5728fcd9b616f
SeongSoo Jeon <stome159@naver.com>
2021-02-15 17:57:01 +0900

    HomeActivity, GMap에 네비바 적용 완료

--- app/src/main/java/com/example/clug_bobple/HomeActivity.java
+++ app/src/main/java/com/example/clug_bobple/HomeActivity.java
@@ -1,21 +1,63 @@
 package com.example.clug_bobple;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private DrawerLayout home_layout;
+    private View navigation;
+    private ImageView menu_button;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        home_layout = (DrawerLayout)findViewById(R.id.home_layout);
+        navigation = (View)findViewById(R.id.navigation);
+        menu_button = (ImageView)findViewById(R.id.menu_button);
+        menu_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                home_layout.openDrawer(navigation);
+            }
+        });
+
+        home_layout.setDrawerListener(listener);
+        navigation.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
     }
 
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
 }
