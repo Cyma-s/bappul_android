e14a9023f4967bcdef669f20c6f0e6009becc346
JaeHoon Kim <choco730522@gmail.com>
2021-02-18 02:33:18 +0900

    밥약글쓰기 xml 만듬

---
+++ app/src/main/java/com/example/clug_bobple/bapyak_write.java
@@ -0,0 +1,14 @@
+package com.example.clug_bobple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bapyak_write extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bapyak_write);
+    }
+}
